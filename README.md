2a78abaec1c7c4eb5127e32fb30690ff12852b34
LubixDev <76389364+LubixDev@users.noreply.github.com>
2023-03-13 15:00:38 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# 3D rotating triangle
+
+##Installing
+
+**Python 3.9 or higher is required**
+```
+# Linux/macOS
+
+git clone https://github.com/LubixDev/3D-rotating-triangle
+pip3 install -r requirements.txt
+python3 main.py
+
+# Windows
+
+git clone https://github.com/LubixDev/3D-rotating-triangle
+pip install -r requirements.txt
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,110 @@
+import pygame
+import numpy as np
+from math import *
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+
+WIDTH, HEIGHT = 800, 600
+pygame.display.set_caption("  ")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+scale = 100
+
+circle_pos = [WIDTH/2, HEIGHT/2]  # x, y
+
+angle_x = 0
+angle_y = 0
+angle_z = 0
+
+points = []
+
+# all the triangle vertices
+points.append(np.matrix([0, 0, 1]))
+points.append(np.matrix([0, 0, 1]))
+points.append(np.matrix([0,  0, 1]))
+points.append(np.matrix([0, 0, 1]))
+points.append(np.matrix([-1, -1, -1]))
+points.append(np.matrix([1, -1, -1]))
+points.append(np.matrix([1, 1, -1]))
+points.append(np.matrix([-1, 1, -1]))
+
+
+projection_matrix = np.matrix([
+    [1, 0, 0],
+    [0, 1, 0]
+])
+
+
+projected_points = [
+    [n, n] for n in range(len(points))
+]
+
+
+def connect_points(i, j, points):
+    pygame.draw.line(
+        screen, WHITE, (points[i][0], points[i][1]), (points[j][0], points[j][1]))
+
+
+clock = pygame.time.Clock()
+while True:
+
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+
+    # update stuff
+
+    rotation_z = np.matrix([
+        [cos(angle_z), -sin(angle_z), 0],
+        [sin(angle_z), cos(angle_z), 0],
+        [0, 0, 1],
+    ])
+
+    rotation_y = np.matrix([
+        [cos(angle_y), 0, sin(angle_y)],
+        [0, 1, 0],
+        [-sin(angle_y), 0, cos(angle_y)],
+    ])
+
+    rotation_x = np.matrix([
+        [1, 0, 0],
+        [0, cos(angle_x), -sin(angle_x)],
+        [0, sin(angle_x), cos(angle_x)],
+    ])
+    angle_x += 0.01
+    angle_y += 0.01
+    angle_z += 0.01
+
+    screen.fill(BLACK)
+    # drawining stuff
+
+    i = 0
+    for point in points:
+        rotated2d = np.dot(rotation_z, point.reshape((3, 1)))
+        rotated2d = np.dot(rotation_y, rotated2d)
+        rotated2d = np.dot(rotation_x, rotated2d)
+
+        projected2d = np.dot(projection_matrix, rotated2d)
+
+        x = int(projected2d[0][0] * scale) + circle_pos[0]
+        y = int(projected2d[1][0] * scale) + circle_pos[1]
+
+        projected_points[i] = [x, y]
+        pygame.draw.circle(screen, BLACK, (x, y), 0)
+        i += 1
+
+    for p in range(4):
+        connect_points(p, (p+1) % 4, projected_points)
+        connect_points(p+4, ((p+1) % 4) + 4, projected_points)
+        connect_points(p, (p+4), projected_points)
+
+    pygame.display.update()
